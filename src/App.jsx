1f1944687a9d54c15e8691e222fa2d8f86b46ef8
The-Tourist-170 <ayushya.official@gmail.com>
2025-02-14 15:48:37 +0530

    Setup Vite and Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-extrabold italic text-cyan-700'>
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
